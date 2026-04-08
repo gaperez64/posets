d3c0f2e05c388bee8dd6e1590a2db108ef0b3f11
gaperez64 <gaperez64@gmail.com>
2026-04-08 09:39:30 +0200

    now also formatted

--- include/posets/utils/sharingtrie.hh
+++ include/posets/utils/sharingtrie.hh
@@ -243,8 +243,7 @@ namespace posets::utils {
         }
         // Set up the generation-stamp cache and DFS stack for dominates()
         this->nxt_color = nxt_color;
-        this->dominates_stamp.assign (this->dim *
-                                      (static_cast<size_t> (nxt_color) * 2), 0U);
+        this->dominates_stamp.assign (this->dim * (static_cast<size_t> (nxt_color) * 2), 0U);
         this->dominates_gen = 0U;
         this->dominates_stack.reserve (this->dim);
       }
